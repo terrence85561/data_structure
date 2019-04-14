59d92900004030ffa20aa531de685e76ea591655
terrence85561 <tianyi4@ualberta.ca>
2019-04-14 16:44:32 -0600

    done stack

--- java/src/com/company/Stack.java
+++ java/src/com/company/Stack.java
@@ -18,40 +18,53 @@ public class Stack {
     public boolean isEmpty(){
         return index == 0;
     }
+
     public void push(int data){
-        if(index == stackArr.length){
-            resizeArr(stackArr.length*2);
+        if(index==stackArr.length){
+            resize(stackArr.length*2);
         }
         stackArr[index] = data;
         index++;
     }
 
-
     public int pop(){
         index--;
-        int result = stackArr[index];
-        stackArr[index] = 0;
-        if(index>0 && index == stackArr.length/4){
-            resizeArr(stackArr.length/2);
+        if(index>0 && index==stackArr.length/4){
+            resize(stackArr.length/2);
         }
+        int result = stackArr[index];
+
         return result;
+
     }
 
-
-    public void resizeArr(int size){
+    public void resize(int size){
         int[] newArr = new int[size];
-        for (int i = 0; i < stackArr.length;i++){
+        for(int i=0; i<index; i++){
             newArr[i] = stackArr[i];
         }
         stackArr = newArr;
 
     }
 
+
     public static void main(String[] args) {
         Stack stack = new Stack();
         stack.push(1);
         stack.push(2);
-        int data = stack.pop();
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+        stack.push(7);
+        stack.push(8);
+        System.out.println(Arrays.toString(stack.getStack()));
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
+        stack.pop();
         System.out.println(Arrays.toString(stack.getStack()));
 
     }
