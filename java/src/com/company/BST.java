f390b8cfda44d32efdc12edc44fe9c24f672667a
terrence85561 <tianyi4@ualberta.ca>
2019-06-08 23:35:23 -0700

    BST recursively floor and ceiling

--- java/src/com/company/BST.java
+++ java/src/com/company/BST.java
@@ -56,10 +56,75 @@ public class BST <Key extends Comparable<Key>, Value> {
         return x;
     }
 
+    public Key min(){
+        return min(root).key;
+    }
 
+    private Node min(Node x){
+        if(x.left == null) return x;
+        return min(x.left);
+    }
 
+    public Key max(){
+        return max(root).key;
+    }
 
+    private Node max(Node x){
+        if(x.right == null) return x;
+        return max(x.right);
+    }
 
+    public Key floor(Key key){
+        /*
+            Case 1 : if a given key is less than the key at the root of BST, then the floor
+            of key must be in the left subtree.
+            Case 2 : If key is greater than the key at the root, then the floor of key could
+            be in the right subtree, but only if there is a key smaller than or equal to key
+            in the right subtree.
+            Case 3: if not, or if key is equal to the key at the root, then the key at the root
+            is the floor of key.
+         */
+        Node x = floor(root, key);
+        if(x == null) return null;
+        return x.key;
+    }
+
+    private Node floor(Node x, Key key){
+        if(x==null) return null;
+        int cmp = key.compareTo(x.key);
+        if(cmp==0) return x;
+        if(cmp < 0){
+            return floor(x.left, key);
+        }
+        Node right = floor(x.right, key);
+        if(right!=null){
+            return right;
+        }else{
+            return x;
+        }
+
+    }
+
+    public Key ceiling(Key key){
+        Node x = ceiling(root, key);
+        if(x == null) return null;
+        return x.key;
+    }
+
+    private Node ceiling(Node x, Key key){
+        if(x == null) return null;
+        int cmp = key.compareTo(x.key);
+        if(cmp==0) return x;
+        if(cmp > 0){
+            return ceiling(x.right, key);
+        }
+        Node left = ceiling(x.left, key);
+        if(left!=null){
+            return left;
+        }else{
+            return x;
+        }
+    }
 
 
 }
